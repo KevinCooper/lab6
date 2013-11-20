063bb87bb0076298120299b081a7a234200553b5
Kevin Cooper <boo2atfoo@gmail.com>
2013-11-20 11:14:49 -0700

    Added in the turn subroutines and initialize clock subroutine

--- main.c
+++ main.c
@@ -5,22 +5,7 @@
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-    P1DIR |= BIT2;                // TA0CCR1 TA0.0 on P1.2
-    P1SEL |= BIT2;                // TA0CCR1 TA0.0 on P1.2
-    P2DIR |= BIT1;					//Set up 2.0
-    P2SEL |= BIT1;					//Set up 2.0
-        TACTL &= ~MC1|MC0;            // stop timer A0
 
-        TACTL |= TACLR;                // clear timer A0
-
-        TACTL |= TASSEL1;           // configure for SMCLK
-
-        TACCR0 = 200;                // set signal period to 200 clock cycles (~100 microseconds)
-        TACCR1 = 10;                // set duty cycle to 50/200 (25%)
-
-        TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-        TACTL |= MC0;                // count up
         while (1) {
                 __delay_cycles(1000000);
                 TACCR1 = 10;            // set duty cycle to 50/100 (50%)
@@ -29,4 +14,27 @@ int main(void) {
             }
 
 	return 0;
+}
+
+void setupClock(){
+    TACTL &= ~MC1|MC0;            // stop timer A0
+    TACTL |= TACLR;                // clear timer A0
+    TACTL |= TASSEL1;           // configure for SMCLK
+    TACCR0 = 200;                // set signal period to 200 clock cycles (~100 microseconds)
+    TACCR1 = 10;                // set duty cycle to 50/200 (25%)
+    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+    TACTL |= MC0;                // count up
+}
+
+void turnLeft(){
+    P1DIR |= BIT2;                // TA0CCR1 on P1.2
+    P1SEL |= BIT2;
+}
+void turnRight(){
+	P2DIR |= BIT1;					//Set up P2.1
+	P2SEL |= BIT1;					//Set up P2.1
+}
+
+void moveForward(){
+
 }
