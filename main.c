4dda12fe476b51c128e33d89496f14d4f50014a4
Kevin Cooper <boo2atfoo@gmail.com>
2013-11-21 03:24:43 -0700

    Trying to test the functionality of the move forward, but setting the CCR's seems to work differently in this method than the initMethod and then calling the individual wheels.  Confusing indeed

--- main.c
+++ main.c
@@ -8,14 +8,7 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
     initRobot();
         while (1) {
-        	turnLeftWheel();
-        	__delay_cycles(1000000);
-        	stopLeft();
-        	__delay_cycles(1000000);
-        	turnRightWheel();
-        	__delay_cycles(1000000);
-        	stopRight();
-        	__delay_cycles(1000000);
+        		moveForward(8,3);
             }
 	return 0;
 }
