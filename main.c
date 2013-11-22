b48949933a13b55f557754a66209553580ca9830
Kevin Cooper <boo2atfoo@gmail.com>
2013-11-22 02:29:02 -0700

    Verified the forward moving functionality of the robot for both wheels.  In addition, the stop functionality is verified for both wheels

--- main.c
+++ main.c
@@ -4,16 +4,18 @@
 /*
  * main.c
  */
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-    initRobot();
-    turnRightWheel();
-    //moveForward(50,50);
-        while (1) {
-        		//moveForward(80,30);
-      //  		turnLeftWheel();
-        		//turnRightWheel();
-            }
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	initRobot();
+	while (1) {
+		turnRightWheel();
+		turnLeftWheel();
+		__delay_cycles(1000000);
+		stopRight();
+		stopLeft();
+		__delay_cycles(1000000);
+	}
 	return 0;
 }
 
