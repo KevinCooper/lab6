a424379a0ed94a4e438c15740ca6cc8ee95093f1
Kevin Cooper <boo2atfoo@gmail.com>
2013-11-22 02:56:57 -0700

    Full functionality tested and working

--- main.c
+++ main.c
@@ -8,16 +8,30 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	initRobot();
-	//turnLeftWheelBack();
-	//moveBackward(40, 40);
 	while (1) {
-		leftTurn();
+		moveForward(50, 50);
+		__delay_cycles(500000);
+		stop();
 		__delay_cycles(1000000);
-		rightTurn();
-		__delay_cycles(1000000);
+		moveBackward(50, 50);
+		__delay_cycles(500000);
+		stop();
+		__delay_cycles(2000000);
 		halfLeft();
+		__delay_cycles(500000);
+		stop();
 		__delay_cycles(1000000);
 		halfRight();
+		__delay_cycles(500000);
+		stop();
+		__delay_cycles(1000000);
+		leftTurn();
+		__delay_cycles(500000);
+		stop();
+		__delay_cycles(1000000);
+		rightTurn();
+		__delay_cycles(500000);
+		stop();
 		__delay_cycles(1000000);
 	}
 	return 0;
